094f1381b3e476a069f96256ac319ba90aab9f6e
Munoz <152181111+Kvrmea@users.noreply.github.com>
2024-11-12 16:17:21 +0100

    étape 5

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
 // étape 1 et 2
 // Fonction qui permet de voir le nombre d'allumette
-function nbrAlummette () {
+function jeuAllumettes () {
     let totalAllumette = 50
+    let nombreJoueurs = Number(prompt("Combien de joueurs vont participer ?"))
+
+    // Vérifie l'entrée pour le nombre de joueurs
+    while (isNaN(nombreJoueurs) || nombreJoueurs < 2) {
+        nombreJoueurs = Number(prompt("Veuillez entrer un nombre de joueurs valide (au moins 2)."))
+    }
+
     let joueur = 1
 
    while (totalAllumette > 0) {
     // On demande à l'utilisateur combien d'allumettes il souhaite retirer
-    let removeAlummettes = Number(prompt(`Joueur ${joueur}, Combien d'allumettes veut tu retirer ? Il en reste ${totalAllumette}. (Tu peux retirer entre 1 et 6 allumettes)`))
+    let removeAllummettes = Number(prompt(`Joueur ${joueur}, Combien d'allumettes veut tu retirer ? Il en reste ${totalAllumette}. (Tu peux retirer entre 1 et 6 allumettes)`))
 
     // Vérifie si la saisie est validé 
-    if (isNaN(removeAlummettes) || removeAlummettes <= 0 || removeAlummettes > 6) {
+    if (isNaN(removeAllummettes) || removeAllummettes <= 0 || removeAllummettes > 6) {
         console.log("Retire entre 1 et 6 allumettes.")
         continue
     }
 
     // Vérifie si l'utilisateur essaie de retirer + d'allumette qu'il en reste
-    if (removeAlummettes > totalAllumette) {
+    if (removeAllummettes > totalAllumette) {
         console.log("Tu ne peux pas enlevé plus d'allumette qu'il n'en reste")
         continue
     }
 
     // Soustrait le nombre d'allumette demandé 
-    totalAllumette -= removeAlummettes
+    totalAllumette -= removeAllummettes
 
     // Affiche le nombre d'allumette restantes
     console.log(`Il reste ${totalAllumette} allumettes.`)
@@ -33,9 +40,9 @@ function nbrAlummette () {
     }
 
     // Change de joueur pour le tour suivant
-    joueur = joueur === 1 ? 2 : 1
+    joueur = (joueur % nombreJoueurs) + 1
     
    }
 }
 
-nbrAlummette()
+jeuAllumettes()
