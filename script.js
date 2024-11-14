51a766ef19fb35b324625a83766e8c99577573ae
Munoz <152181111+Kvrmea@users.noreply.github.com>
2024-11-14 11:50:06 +0100

    ajout d'événemment + fonction showMessage

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ let totalAllumette = 50
 let nombreJoueurs = 0
 let joueur = 1
 
+// Sélection des éléments HTML pour le message en direct
+const textInLive = document.getElementById("texteEnDirect")
+const zoneMessage = document.getElementById("messageUser")
+
 // Fonction qui lance le jeu
 function startGame() {
     const playerInput = document.getElementById("players").value
@@ -22,27 +26,31 @@ function startGame() {
     document.getElementById("errorMessage").textContent = ""
 }
 
+// écouteur d'événement pour cliqué sur "Commencer le jeu"
+document.getElementById("startBtn").addEventListener("click", startGame)
+
 // Fonction pour retirer les Allumettes
 function removeMatches() {
     const removeAllummettes = Number(document.getElementById("removeMatches").value)
-
+    
     // Vérifie que le nombre d'allumettes est valide (entre 1 et 6)
     if (isNaN(removeAllummettes) || removeAllummettes < 1 || removeAllummettes > 6) {
         document.getElementById("errorMessage").textContent = "Retirez entre 1 et 6 allumettes."
         return
     }
-
+    
     // Vérifie qu'il reste assez d'allumettes pour continuer
     if (removeAllummettes > totalAllumette) {
         document.getElementById("errorMessage").textContent = "Tu ne peux pas enlever plus d'allumettes qu'il n'en reste."
         return
     }
-
+    
     // MAJ du jeu
     totalAllumette -= removeAllummettes
     document.getElementById("remainingMatches").textContent = totalAllumette
+    document.getElementById("removeMatches").value = ""
     document.getElementById("errorMessage").textContent = ""
-
+    
     // Vérifie si toutes les allumettes sont prises, + fin du jeu
     if (totalAllumette === 0) {
         document.getElementById("gameStatus").textContent = `Joueur ${joueur}! Tu n'as pas de chance ! C'est perdu !`
@@ -51,11 +59,29 @@ function removeMatches() {
         document.getElementById("restartButton").style.display = "inline"
         return
     }
-
+    
     // Change de joueur
     joueur = (joueur % nombreJoueurs) + 1
     document.getElementById("playerNumber").textContent = joueur    
 }
+
+// Ajout d'un écouteur d'évenement pour cliquer sur "Entrée" pour la button Retirer
+document.getElementById("removeMatches").addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        removeMatches()
+    }
+})
+
+// Fonction pour afficher le message en direct 
+function showMessage (message) {
+    zoneMessage.textContent = message
+}
+
+// MAJ en direct pendant la saisie
+document.getElementById("removeMatches").addEventListener("input", function(event) {
+    const texteSaisi = event.target.value
+    textInLive.textContent = `Ton choix est de ${texteSaisi} allumettes` || "Votre choix apparaîtra ici"
+})
 
 // Fonction qui restart le jeu
 function restartGame() {
@@ -89,3 +115,5 @@ function toggleRules() {
         rulesButton.textContent = "Règles du Jeu";
     }
 }
+
+document.getElementById("rulesButton").addEventListener("click", toggleRules)
